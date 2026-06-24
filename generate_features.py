d00f4457e4e15f55e4b22e1c76a33ca716b71222
gassug2 <gassug2@gmail.com>
2026-06-24 08:17:23 -0700

    Fix generate_features.py

--- generate_features.py
+++ generate_features.py
@@ -5,7 +5,7 @@ from pathlib import Path
 content = f"""\
 HAS_AUDIOVIDEO = {os.getenv("FFMC_AUDIOVIDEO", "True")}
 HAS_IMAGE = {os.getenv("FFMC_IMAGE", "True")}
-HAS_DOCUMENT {os.getenv("FFMC_DOCUMENT", "True")}
+HAS_DOCUMENT = {os.getenv("FFMC_DOCUMENT", "True")}
 """
 
 Path("ffmulticonverter/features.py").write_text(content)
